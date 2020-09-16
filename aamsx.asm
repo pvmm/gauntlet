5cdc2a7ea43bb0d6c8ec9fc622e3b9e3b1194284
Giovanni Nunes <giovanni.nunes@gmail.com>
2020-09-16 09:37:10 -0300

    Rename AAMSX related files and reformat 'aamsx.asm' file.

--- aamsx.asm
+++ aamsx.asm
@@ -1,65 +1,62 @@
-section code
-
+;section code
 
 MSXPalet:
-	db 00h,0,00h,0,11h,6,33h,7,17h,1,27h,3,51h,1,27h,6
-	db 71h,1,73h,3,61h,6,64h,6,11h,4,65h,2,55h,5,77h,7
-
-
+            db 00h,0,00h,0,11h,6,33h,7,17h,1,27h,3,51h,1,27h,6
+	       db 71h,1,73h,3,61h,6,64h,6,11h,4,65h,2,55h,5,77h,7
 
 StartLogo:
-                    xor		a
-                    ld		(CLIKSW),a
-                    ld		(CSRSW),a
-                    xor		a
-                    ld		(FORCLR),a
-                    ld		(BAKCLR),a
-                    ld		(BDRCLR),a
+            xor     a
+            ld		(CLIKSW),a
+            ld		(CSRSW),a
+            xor	a
+            ld		(FORCLR),a
+            ld		(BAKCLR),a
+            ld		(BDRCLR),a
 
-		    call	VIS_OFF
-		    xor         a
-                    call        VER_PAGE
-		    ld 		hl, MSXPalet
-		    call        PutPal
+            call	VIS_OFF
+            xor     a
+            call    VER_PAGE
+            ld 	hl, MSXPalet
+            call    PutPal
 	
-	            ld          a,2
-		    call	CHGMOD
+	       ld      a,2
+		  call	CHGMOD
 
-                    ; Logo!!
-                    ld		hl,AAMSX_PAT+8
-                    ld		de,0c000h
-        	    call    	UnTCF
-		    ld		hl,0c000h
-		    ld 		de,0
-		    ld		bc,3*0800h
-		    call	LDIRVM
+            ; Logo!!
+            ld		hl,AAMSX_PAT+8
+            ld		de,0c000h
+        	  call    nTCF
+		  ld		hl,0c000h
+		  ld 	de,0
+		  ld		bc,3*0800h
+		  call	LDIRVM
 
-                    ld		hl,AAMSX_COL+8
-                    ld		de,0c000h
-        	    call    	UnTCF
-		    ld		hl,0c000h
-		    ld 		de,02000h
-		    ld		bc,3*0800h
-		    call	LDIRVM
+            ld		hl,AAMSX_COL+8
+            ld		de,0c000h
+        	  call   	UnTCF
+		  ld		hl,0c000h
+		  ld 	de,02000h
+		  ld		bc,3*0800h
+		  call	LDIRVM
 
-                    ld		bc,260
-.loop:		    push	bc
-		    ld 		a,8
-                    call	SNSMAT
-                    pop        	bc
-		    and 	1
-		    ret		z
-                    ei
-                    halt
-		    dec		bc
-		    ld		a,c
-		    or		b
-                    jr 		nz,.loop
+            ld		bc,260
+.loop:      push	bc
+	       ld 	a,8
+            call	SNSMAT
+            pop    	bc
+		  and 	1
+		  ret	z
+            ei
+            halt
 
-                    ret
+            dec	bc
+            ld		a,c
+            or		b
+            jr 	nz,.loop
 
+            ret
 
 AAMSX_COL:
-                    incbin "AAMSX.COL"
+            incbin "aamsx.col"
 AAMSX_PAT:
-                    incbin "AAMSX.PAT"
+            incbin "aamsx.pal"
